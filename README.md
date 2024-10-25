294a8f208da66b15c43703bf534c705c57e89738
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-10-25 09:38:30 +0500

    Add Books component and template for that

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Книги для книжного клуба
 
 ## Технологии
-- Flask for  RESTful API
+- Flask (for RESTful API)
 - Vue (JS framework for the interface)
 - Axios library (for the requests)
 - Bootstrap (for the beauty)
---
+++ client/src/components/Books.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10">
+                <h1>Books</h1>
+                <hr><br><br>
+                <button type="button" class="btn btn-success btn-sm">Add book</button>
+                <br><br>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Desciption</th>
+                            <th scope="col">Read?</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Ttile1</td>
+                            <td>Author1</td>
+                            <td>Descr1</td>
+                            <td>No</td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-warning btn-sm">Udpate</button>
+                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
