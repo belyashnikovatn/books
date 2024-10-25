eaa9bdf6a6f2e340ac76a7c55def2c5439991041
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-10-25 11:07:40 +0500

    Add GET all Books

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -2,26 +2,29 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-10">
-                <h1>Books</h1>
+                <h1>Книги</h1>
                 <hr><br><br>
-                <button type="button" class="btn btn-success btn-sm">Add book</button>
+                <button type="button" class="btn btn-success btn-sm">Добавить книгу</button>
                 <br><br>
                 <table class="table table-hover">
                     <thead>
                         <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Author</th>
-                            <th scope="col">Desciption</th>
-                            <th scope="col">Read?</th>
+                            <th scope="col">Название</th>
+                            <th scope="col">Автор</th>
+                            <th scope="col">Описание</th>
+                            <th scope="col">Прочли?</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Ttile1</td>
-                            <td>Author1</td>
-                            <td>Descr1</td>
-                            <td>No</td>
+                        <tr v-for="(book, index) in books" :key="index">
+                            <td>{{ book.title }}</td>
+                            <td>{{ book.author }}</td>
+                            <td>{{ book.description }}</td>
+                            <td>
+                                <span v-if="book.read">Да</span>
+                                <span v-else>Нет</span>
+                            </td>
                             <td>
                                 <div class="btn-group" role="group">
                                     <button type="button" class="btn btn-warning btn-sm">Udpate</button>
@@ -36,3 +39,29 @@
     </div>
 </template>
 
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            books: [],
+        };
+    },
+    methods: {
+        getBooks() {
+            const path = 'http://localhost:5001/books';
+            axios.get(path)
+                .then((res) => {
+                    this.books = res.data.books;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+    },
+    created() {
+        this.getBooks();
+    },
+};
+</script>
--- server/app.py
+++ server/app.py
@@ -6,10 +6,39 @@ app.config.from_object(__name__)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+BOOKS = [
+    {
+        'title': 'Повелитель мух',
+        'author': 'У.Голдинг',
+        'description': 'История благовоспитанных мальчиков, внезапно оказавшихся на необитаемом острове.',
+        'read': True
+    },
+    {
+        'title': 'Смерти.net',
+        'author': 'Татьяна Замировская',
+        'description': 'Антиутопия о об интернете для мертвых, всеобщем цифровом воскресении и изоляции',
+        'read': True
+    },
+    {
+        'title': 'Опосредованно',
+        'author': 'А.Сальников',
+        'description': 'Альтернативная реальность, где стихи - это не просто текст, а настоящий наркотик',
+        'read': False
+    },
+]
+
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
 
 
 if __name__ == '__main__':
